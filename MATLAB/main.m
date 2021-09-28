15fc4db63bdc1019070cdf14a87e5de070f7c7c7
Hugo Pérez <47252665+Hugondon@users.noreply.github.com>
2021-09-27 21:39:31 -0500

    Se agregó formato a programas

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -9,23 +9,25 @@ clear, clc, clear all
 
 %% Function Handles
 
-declareRobot        =   @declareRobot;
-setWaypoints        =   @setWaypoints;
-getTrajectoryTimes  =   @getTrajectoryTimes;
+declareRobot = @declareRobot;
+setWaypoints = @setWaypoints;
+getTrajectoryTimes = @getTrajectoryTimes;
 
 %% Loading Robot
 
 %Get the object robot, the number of joints and endeffector
-[robot,numJoint,endEffector]=declareRobot("universalUR5");
+[robot, numJoint, endEffector] = declareRobot("universalUR5");
 
 %Get Joint angles Home position and TCP position and
 %orientation
-load UR5positions
 
+jointHomeAngles = [-1.055051532830572; -0.922930108454602; -2.274513081199010; -1.514247659030281; 1.572192590196492; 0.515570261039125];
+toolHomeOrientation = [0; 3.141592653589793; 0];
+toolHomePosition = [0; -0.270000000000000; 0.380000000000000];
 %% Set Inverse Kinematics
 
 % Define IK
-ik = inverseKinematics('RigidBodyTree',robot);
+ik = inverseKinematics('RigidBodyTree', robot);
 ikWeights = [1 1 1 1 1 1];
 ikInitGuess = jointHomeAngles;
 
@@ -34,58 +36,52 @@ ikInitGuess = jointHomeAngles;
 %The function return an array of poses(waypoints) the number
 %of waypoints and the magnitude between consecutive
 %waypoints.
-[waypoints,numberWaypoints,magnitudeDistances]=setWaypoints();
+[waypoints, numberWaypoints, magnitudeDistances] = setWaypoints();
 
 %% Trajectory sample time
 
 % TCP Speed(Defined by user)
-tcpSpeed=3; %[m/s]
+tcpSpeed = 3; %[m/s]
 
 % Number of Intermediate Waypoints(Defined by user)
-nIntermediateWaypoints=50;
+nIntermediateWaypoints = 50;
 
 % Get the cummulative sum of the magnitudes of the distance
-csMagnitudeDistances=cumsum([0,magnitudeDistances]);
+csMagnitudeDistances = cumsum([0, magnitudeDistances]);
 
 % Get the times between (main) Waypoints dividing by TCP speed
-timesBetweenWaypoints=csMagnitudeDistances/tcpSpeed;
+timesBetweenWaypoints = csMagnitudeDistances / tcpSpeed;
 
-[trajTimes,ts]=getTrajectoryTimes(nIntermediateWaypoints,csMagnitudeDistances);
-
-
+[trajTimes, ts] = getTrajectoryTimes(nIntermediateWaypoints, csMagnitudeDistances);
 
 %trajTimes = 0:ts:timesBetweenWaypoints(end);
 %timesBetweenWaypoints=0:10:20;
 %ts = 0.2;
 %trajTimes = 0:ts:timesBetweenWaypoints(end);
 
-
 %% Trajectory
 
-for count = 1:numberWaypoints-1
-    timeInterval = timesBetweenWaypoints(count:count+1);
+for count = 1:numberWaypoints - 1
+    timeInterval = timesBetweenWaypoints(count:count + 1);
     trajInterval = timeInterval(1):ts(count):timeInterval(2);
-    
+
     % Find the transforms from trajectory generation
-    [T,vel,acc] = transformtraj(waypoints(:,:,count),waypoints(:,:,count+1),timeInterval,trajInterval);
-    
-    
-    for idx = 1:numel(trajInterval) 
+    [T, vel, acc] = transformtraj(waypoints(:, :, count), waypoints(:, :, count + 1), timeInterval, trajInterval);
+
+    for idx = 1:numel(trajInterval)
         % Solve IK
-        tgtPose = T(:,:,idx);
-        [config,info] = ik(endEffector,tgtPose,ikWeights,ikInitGuess);
+        tgtPose = T(:, :, idx);
+        [config, info] = ik(endEffector, tgtPose, ikWeights, ikInitGuess);
         ikInitGuess = config;
 
         % Show the robot
         %show(robot,config,'Frames','off','PreservePlot',false);
-        show(robot,config,'Frames','off','PreservePlot',false);
+        show(robot, config, 'Frames', 'off', 'PreservePlot', false);
         title(['Trajectory at t = ' num2str(trajInterval(idx))])
         %Get the desired View
         view([-0.6 -0.6 0.2]);
         %view(-45, 0.5)
         drawnow
     end
-    
+
 end
-
-
--- MATLAB/setWaypoints.m
+++ MATLAB/setWaypoints.m
@@ -1,36 +1,34 @@
-%% Set the Waypoints, get the number of waypoints and the diatance betweenthe consecutive waypoints. 
+%% Set the Waypoints, get the number of waypoints and the diatance betweenthe consecutive waypoints.
 
-function [waypoints,number_waypoints,magnitude_distances] = setWaypoints()
-%Get positions declare w.r.t Global reference
-positions=      [-0.5,-0.3,0.4;...
-                0,-0.3,0.2;...
-                0.5,-0.3,0.4;...
-                -0.5,-0.3,0.4];
-%Get orientations(in rotation vector)        
-orientations=   [0,1,0,pi;...
-                0,1,0,pi;...
-                0,1,0,pi;...
-                0,1,0,pi];
+function [waypoints, number_waypoints, magnitude_distances] = setWaypoints()
+    %Get positions declare w.r.t Global reference
+    positions = [-0.5, -0.3, 0.4; ...
+                0, -0.3, 0.2; ...
+                0.5, -0.3, 0.4; ...
+                -0.5, -0.3, 0.4];
+    %Get orientations(in rotation vector)
+    orientations = [0, 1, 0, pi; ...
+                    0, 1, 0, pi; ...
+                    0, 1, 0, pi; ...
+                    0, 1, 0, pi];
 
-%Get number of positions
-number_waypoints=size(positions,1);
+    %Get number of positions
+    number_waypoints = size(positions, 1);
 
-% Reserve memory
-waypoints=zeros(4,4,number_waypoints);
+    % Reserve memory
+    waypoints = zeros(4, 4, number_waypoints);
 
-% Process variables to get the magnitude of the distance
-pos1=positions(2:end,:);
-pos2=positions(1:end-1,:);
+    % Process variables to get the magnitude of the distance
+    pos1 = positions(2:end, :);
+    pos2 = positions(1:end - 1, :);
 
-% Get the magnitude of the distance and store it on a matrix
-magnitude_distances=vecnorm((pos1-pos2)');
-% Transpose the matrix with the magnitude of the distances
-%magnitude_distances=magnitude_distances';
+    % Get the magnitude of the distance and store it on a matrix
+    magnitude_distances = vecnorm((pos1 - pos2)');
+    % Transpose the matrix with the magnitude of the distances
+    %magnitude_distances=magnitude_distances';
 
-clear pos1 pos2
+    clear pos1 pos2
 
-
-% Get poses with positions and orientations
-waypoints(:,:,:)=pagemtimes(trvec2tform(positions(:,:)),axang2tform(orientations(:,:)));
+    % Get poses with positions and orientations
+    waypoints(:, :, :) = pagemtimes(trvec2tform(positions(:, :)), axang2tform(orientations(:, :)));
 end
-
