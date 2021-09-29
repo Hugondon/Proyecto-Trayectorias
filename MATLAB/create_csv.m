99a9e546b9f140b9903ccf938a00a7ebd45b8f14
Hugo Pérez <47252665+Hugondon@users.noreply.github.com>
2021-09-28 23:34:51 -0500

    Escribir archivos CSV desde MATLAB

---
+++ MATLAB/create_csv.m
@@ -0,0 +1,21 @@
+clear, clc, clear all
+
+FILENAME = 'coordinates.csv';
+x_coordinates_m = 1:5;
+y_coordinates_m = 10:15;
+z_coordinates_m = 20:25;
+angles_radians = 30:35;
+
+% Mustn't be changed to [] although warning says so.
+column_names = {"X coordinate [m]", "Y coordinate [m]", "Z coordinate [m]", "Angle [radians]"};
+
+% Writes row containing column headers.
+writecell(column_names, FILENAME, 'WriteMode', 'overwrite');
+
+number_of_columns = size(angles_radians, 2);
+
+for count = 1:number_of_columns - 1
+    data = [x_coordinates_m(count) y_coordinates_m(count) z_coordinates_m(count) angles_radians(count)];
+%     Writes data as new row.
+    writematrix(data, FILENAME, 'WriteMode', 'append');
+end
