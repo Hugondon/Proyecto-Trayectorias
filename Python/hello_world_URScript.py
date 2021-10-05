293db6cad42be55dc0d952e668d41b5421e50387
Hugo Perez <hugo.rpc9@gmail.com>
2021-10-04 21:25:09 -0500

    primer approach a movel

--- Python/hello_world_URScript.py
+++ Python/hello_world_URScript.py
@@ -1,9 +1,25 @@
 import os
 
-# No se actualiza automáticamente en la interfaz de Polyscope
+# No se actualiza automáticamente en la interfaz de Polyscope el archivo que se modifica
+# Hacer objeto para poses ?
+# Typing a funciones que creemos
+# Argumentos opcionales
 
 # FILENAME = 'hello_world.script'
+FILENAME = 'movel_1.script'
 URSCRIPT_PATH = '/home/hugo/URSim-5.10.2/programs/Proyecto/Hello_World_Python'
+class Pose:
+    def __init__(self, x, y, z, rx, ry, rz):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.rx = rx
+        self.ry = ry
+        self.rz = rz
+    
+    def generate_pose(self):
+        return f"p[{self.x}, {self.y}, {self.z}, {self.rx}, {self.ry}, {self.rz}]"
+
 
 def function(name, content):
     function_structure = ''
@@ -13,6 +29,9 @@ def function(name, content):
     return function_structure
 def popup_function(msg):
     return f'popup("{msg}", blocking=True)'
+def movel_function(initial_pose):
+    return f'movel({initial_pose})'
+
 
 def main():
     
@@ -21,9 +40,13 @@ def main():
     os.chdir(rf"{URSCRIPT_PATH}")
     # print(os.getcwd())
     """ Generar archivo """
-    with open(FILENAME, 'w') as file:
-        func = function('hola_mundo', popup_function("Nemo es buena peli"))
-        file.write(func)
+    # with open(FILENAME, 'w') as file:
+    #     func = function('hola_mundo', popup_function("Nemo es buena peli"))
+    #     file.write(func)
+    
+    pose_1 = Pose(0.4, -0.3, 0.4, 0, 1.57, 0)
+    print(pose_1.generate_pose())
+    print(movel_function(pose_1.generate_pose()))
     
 if __name__ == '__main__':
     main()
