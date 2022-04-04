44ba61986d602137cfb709a7433552a552129a6d
Adrián Toledano <adrian.toledanog@gmail.com>
2022-04-04 17:25:13 -0500

    Improved interactive point selection
    -Points in surface of CAD can be selected realiably.
    -Points selected can saved in memory.
    -Points selected can be ploted for visual feedback for the user.

--- MATLAB/Generacion_Trayectoria/CAD_procesing/cad_post_processing.m
+++ MATLAB/Generacion_Trayectoria/CAD_procesing/cad_post_processing.m
@@ -90,56 +90,17 @@ function click(~,~,handle,gm,msh,nodes)
     userSelectedNode=cursorInfo.Position';
     % Get ID from user selected node
     userSelectedNodeID=findNodes(msh,'nearest',userSelectedNode);
-%     % Find the nearest Face from the user selected point
-%     nearFaceID=nearestFace(gm,userSelectedPoint');
-%     % Get nodes from nearest Face from the user selected point 
-%     %nodesInFaceID=findNodes(msh,'region','Face',nearFaceID);
-%     nodesInFaceID=findNodes(msh,'region','Face',1:gm.NumFaces);
-%     
-%     % Find the ID of the nearest node from the user selected point
-%     nearUserSelectionNodeID=findNodes(msh,'nearest',userSelectedPoint);
-%     % Find the nearest node
-%     nearUserSelectionNode=nodes(:,nearUserSelectionNodeID);
-%     % Get the distance fromthe user selected point to the nearest node
-%     radiusSearchSphere=norm(userSelectedPoint-nearUserSelectionNode);
-%     %Find the ID of the nodes within the search sphere
-%     nodesWithinSearchSphereID=findNodes(msh,'radius',userSelectedPoint,radiusSearchSphere);
-%     % Get the ID of the intersection of the nodes within search sphere and the nodes in face
-%     nodesInFaceWithinSearchSphereID=intersect(nodesWithinSearchSphereID,nodesInFaceID);
-%     %Find the nodes within the search sphere
-%     nodesInFaceWithinSearchSphere=nodes(:,nodesInFaceWithinSearchSphereID);
-%     %Get the index of the nearest node in face
-%     [~,indexNearestNode]=min(norm(userSelectedPoint-nodesInFaceWithinSearchSphere));
-%     %Get ID of the nearest node
-%     nearestNodeInFaceID=nodesInFaceWithinSearchSphereID(:,indexNearestNode);
     
-    
-
-    
-    
-    %Print ID of the neares Face
-%     disp(nearFaceID);
-    %Plot nodes of the nearest Face
-%     plot3(  nodes(1,nodesInFaceID),...
-%             nodes(2,nodesInFaceID),...
-%             nodes(3,nodesInFaceID),...
-%             'r*','MarkerSize',10);
-
-%     disp(nearestNodeInFaceID)
-%             %Plot nodes of the nearest Face
-%     plot3(  nodes(1,nearestNodeInFaceID),...
-%             nodes(2,nearestNodeInFaceID),...
-%             nodes(3,nearestNodeInFaceID),...
-%             'r*','MarkerSize',10);
-
-    disp(userSelectedNode)
-            %Plot nodes of the nearest Face
-            %set(handle.p, 'currentaxes', handle.a);
-            figure(2);
-            plot3( userSelectedNode(1,1),...
-            userSelectedNode(2,1),...
-            userSelectedNode(3,1),...
-            'r*','MarkerSize',15);
+    % Display ID of the user selected node
+    disp(userSelectedNodeID)
+    % Select figure to plot
+    figure(2);
+    %Plot Node
+    plot3( userSelectedNode(1,1),...
+    userSelectedNode(2,1),...
+    userSelectedNode(3,1),...
+    'r*','MarkerSize',15);
+    % Save node in csv file
     writematrix(userSelectedNodeID,'puntos.csv','WriteMode', 'append','Delimiter','comma');
 
 end
--- MATLAB/Generacion_Trayectoria/CAD_procesing/test1_multidomain_geometry.m
+++ MATLAB/Generacion_Trayectoria/CAD_procesing/test1_multidomain_geometry.m
@@ -49,27 +49,41 @@ nodesInSurfaceID=findNodes(msh,'region','Face',1:numFaces);
 
 
 %% Test 
-handle.a = axes;
+%handle.a = axes;
 handle.x = nodes(1,:);
 handle.y = nodes(2,:);
 handle.z = nodes(3,:);
+
+
+
+boton.a=figure;
+figure
 % plot in 3D
+%handle.f = figure;
 handle.p = pdemesh(msd,'FaceAlpha',1);
+%handle.btn=uibutton(handle.f);
+%handle.p.VertexNormalsMode='manual';
 %handle.p = pdemesh(msd);
 
 
 hold on
-% handle.dcm=datacursormode;
-% handle.dcm.Enable = 'on';
-
+handle.dcm=datacursormode;
+%handle.dcm.Enable = 'off';
+handle.dcm.SnaptoDataVertex = 'on';
+handle.dcm.DisplayStyle = 'window';
 %handle.dt=datatip(handle.p);
-
 %dcm=datacursormode(handle.a);
 
 
 % add callback when point on plot object 'handle.p' is selected
 % 'click' is the callback function being called when user clicks a point on plot
-handle.p.ButtonDownFcn= {@click,handle,gm,msh,nodes};
+
+boton.a.ButtonDownFcn = {@click,handle,gm,msh,nodes};
+%handle.p.Parent.Parent.KeyPressFcn= {@click,handle,gm,msh,nodes};
+%handle.p.Parent.ButtonDownFcn= {@click,handle,gm,msh,nodes};
+%handle.p.ButtonDownFcn= {@click,handle,gm,msh,nodes};
+
+%handle.dt.ButtonDownFcn= {@click,handle,gm,msh,nodes};
 
 
 
@@ -78,31 +92,31 @@ handle.p.ButtonDownFcn= {@click,handle,gm,msh,nodes};
 function click(~,~,handle,gm,msh,nodes)
     
     % Coordinates of the user selected point
-    userSelectedPoint = handle.a.CurrentPoint(1,:)';
-    
-    
-    % Find the nearest Face from the user selected point
-    nearFaceID=nearestFace(gm,userSelectedPoint');
-    % Get nodes from nearest Face from the user selected point 
-    %nodesInFaceID=findNodes(msh,'region','Face',nearFaceID);
-    nodesInFaceID=findNodes(msh,'region','Face',1:gm.NumFaces);
-    
-    % Find the ID of the nearest node from the user selected point
-    nearUserSelectionNodeID=findNodes(msh,'nearest',userSelectedPoint);
-    % Find the nearest node
-    nearUserSelectionNode=nodes(:,nearUserSelectionNodeID);
-    % Get the distance fromthe user selected point to the nearest node
-    radiusSearchSphere=norm(userSelectedPoint-nearUserSelectionNode);
-    %Find the ID of the nodes within the search sphere
-    nodesWithinSearchSphereID=findNodes(msh,'radius',userSelectedPoint,radiusSearchSphere);
-    % Get the ID of the intersection of the nodes within search sphere and the nodes in face
-    nodesInFaceWithinSearchSphereID=intersect(nodesWithinSearchSphereID,nodesInFaceID);
-    %Find the nodes within the search sphere
-    nodesInFaceWithinSearchSphere=nodes(:,nodesInFaceWithinSearchSphereID);
-    %Get the index of the nearest node in face
-    [~,indexNearestNode]=min(norm(userSelectedPoint-nodesInFaceWithinSearchSphere));
-    %Get ID of the nearest node
-    nearestNodeInFaceID=nodesInFaceWithinSearchSphereID(:,indexNearestNode);
+    %userSelectedPoint = handle.a.CurrentPoint(1,:)';
+    cursorInfo=getCursorInfo(handle.dcm);
+    userSelectedPoint=cursorInfo.Position';
+%     % Find the nearest Face from the user selected point
+%     nearFaceID=nearestFace(gm,userSelectedPoint');
+%     % Get nodes from nearest Face from the user selected point 
+%     %nodesInFaceID=findNodes(msh,'region','Face',nearFaceID);
+%     nodesInFaceID=findNodes(msh,'region','Face',1:gm.NumFaces);
+%     
+%     % Find the ID of the nearest node from the user selected point
+%     nearUserSelectionNodeID=findNodes(msh,'nearest',userSelectedPoint);
+%     % Find the nearest node
+%     nearUserSelectionNode=nodes(:,nearUserSelectionNodeID);
+%     % Get the distance fromthe user selected point to the nearest node
+%     radiusSearchSphere=norm(userSelectedPoint-nearUserSelectionNode);
+%     %Find the ID of the nodes within the search sphere
+%     nodesWithinSearchSphereID=findNodes(msh,'radius',userSelectedPoint,radiusSearchSphere);
+%     % Get the ID of the intersection of the nodes within search sphere and the nodes in face
+%     nodesInFaceWithinSearchSphereID=intersect(nodesWithinSearchSphereID,nodesInFaceID);
+%     %Find the nodes within the search sphere
+%     nodesInFaceWithinSearchSphere=nodes(:,nodesInFaceWithinSearchSphereID);
+%     %Get the index of the nearest node in face
+%     [~,indexNearestNode]=min(norm(userSelectedPoint-nodesInFaceWithinSearchSphere));
+%     %Get ID of the nearest node
+%     nearestNodeInFaceID=nodesInFaceWithinSearchSphereID(:,indexNearestNode);
     
     
     
@@ -110,21 +124,29 @@ function click(~,~,handle,gm,msh,nodes)
     
     
     %Print ID of the neares Face
-    disp(nearFaceID);
-    
+%     disp(nearFaceID);
     %Plot nodes of the nearest Face
 %     plot3(  nodes(1,nodesInFaceID),...
 %             nodes(2,nodesInFaceID),...
 %             nodes(3,nodesInFaceID),...
 %             'r*','MarkerSize',10);
-    disp(nearestNodeInFaceID)
-            %Plot nodes of the nearest Face
-    plot3(  nodes(1,nearestNodeInFaceID),...
-            nodes(2,nearestNodeInFaceID),...
-            nodes(3,nearestNodeInFaceID),...
-            'r*','MarkerSize',10);
 
-    writematrix(nearUserSelectionNodeID,'puntos.csv','WriteMode', 'append','Delimiter','comma');
+%     disp(nearestNodeInFaceID)
+%             %Plot nodes of the nearest Face
+%     plot3(  nodes(1,nearestNodeInFaceID),...
+%             nodes(2,nearestNodeInFaceID),...
+%             nodes(3,nearestNodeInFaceID),...
+%             'r*','MarkerSize',10);
+
+    disp(userSelectedPoint)
+            %Plot nodes of the nearest Face
+            %set(handle.p, 'currentaxes', handle.a);
+            figure(2);
+            plot3( userSelectedPoint(1,1),...
+            userSelectedPoint(2,1),...
+            userSelectedPoint(3,1),...
+            'r*','MarkerSize',15);
+    writematrix(userSelectedPoint,'puntos.csv','WriteMode', 'append','Delimiter','comma');
 
 end
 
