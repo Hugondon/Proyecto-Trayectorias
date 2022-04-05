56867e5746ebf200af341400a8941d49d2c8eb5b
Adrián Toledano <adrian.toledanog@gmail.com>
2022-04-04 19:30:33 -0500

    Ordering and beginnning surface path planning
    -Get the IDs of the nodes which correspond to each face.
    -Get the centroid of each face.
    -Order

---
+++ MATLAB/Generacion_Trayectoria/CAD_procesing/planSurfacePath.m
@@ -0,0 +1,12 @@
+function [outputArg1,outputArg2] = planSurfacePath(mainPointsID,edgeLenght)
+% Get the ID of the Nodes for the route, using the points given by the user
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+%Obtener distancia entre punto de la superficie mas cercano al centoride(nptc) y el punto medio entre los
+%puntos principales(mp) si la distancia es menor a la distancia del nptc a su punto más lejano
+%(transversal de gravedad tdg), se
+%utiliza el tdg, si es más grande se utiliza la distancia entre el ntpc y el mp. De ahi se utilizara
+%una merge sort
+end
+
