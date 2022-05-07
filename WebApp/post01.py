8c2cdb97ca1cd8290b28add8d6c20feb8c307118
DanyaOC <36273088+DanyaOC@users.noreply.github.com>
2022-05-06 21:10:09 -0500

    Flask App - Basic Code

--- WebApp/post01.py
+++ WebApp/post01.py
@@ -1,1 +1,25 @@
+from flask import Flask, redirect, url_for, jsonify, render_template, request
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("dataset.html")
+
+@app.route("/admin", methods=["POST", "GET"])
+def admin():
+    if request.method == "POST":
+        user = request.form["dat"]
+        #return render_template("dataset.html")
+        return redirect(url_for("user", usr = user))
+    else:
+        return render_template("admin.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000)
 
